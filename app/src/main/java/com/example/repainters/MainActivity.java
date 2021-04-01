c0e5c5ae4a1c9dc17631556eec11164e31917d44
ayana_otsu <ayana.otsu5@gmail.com>
2021-04-01 21:06:58 +0900

    Splash screen

--- app/src/main/java/com/example/repainters/MainActivity.java
+++ app/src/main/java/com/example/repainters/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.repainters;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.repainters.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Repainters);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+
+
+
+
+
+
+
+
+
 }
